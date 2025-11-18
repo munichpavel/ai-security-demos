d4e077b4e792f82404be5f382f0cdae70662a267
Paul Larsen <munichpavel@gmail.com>
2025-11-18 09:56:17 +0100

    Switch to shell malware demo

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 # FIXME hack
 import sys
 sys.path.insert(0, 'src')
+from ai_security.chatter_demo import demo as chatter_demo
+from ai_security.malware_demo import demo as malware_demo
 
-from ai_security.chatter_demo import demo
 
+# demo = chatter_demo
+demo = malware_demo
 
 if __name__ == "__main__":
+
     demo.launch()
+
